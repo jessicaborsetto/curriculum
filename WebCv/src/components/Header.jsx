f8cbdd3b7e2190541718b77005e4f12e6951ce68
jessicaborsetto <bjessi2002@gmail.com>
2024-02-12 14:43:19 +0100

    CV

--- WebCv/src/components/Header.jsx
+++ WebCv/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header(){
                             <Button className="rounded-pill homeBtn1"> <a href='https://www.canva.com/design/DAE2vwm6Rl8/rr_4Ko-ekcwFsRc9Lbyd9g/view?utm_content=DAE2vwm6Rl8&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Download CV</a></Button>
                             </Col>
                             <Col  xs={12} sm={12} lg={6}>
-                            <Button className="rounded-pill homeBtn2"><a href='#'>Contact Info</a></Button>
+                            <Button className="rounded-pill homeBtn2"><a href='#contacts'>Contact Info</a></Button>
                             </Col>
                         </Row>
                     
